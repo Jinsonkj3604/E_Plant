aae758c3df0d41b67df79e002b368c183fe4d9c5
jinsonkj <jinsonkj1998@gmail.com>
2023-05-12 07:22:47 +0530

    model_admin_changes

--- eplant_project/eplant_cart/models.py
+++ eplant_project/eplant_cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from eplant_home.models import *
 
 # Create your models here.
+class CartList(models.Model):
+    cart_id = models.CharField(max_length=200,unique=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+class CartItem(models.Model):
+    prod = models.ForeignKey(Product,on_delete=models.CASCADE)
+    cart = models.ForeignKey(CartList,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    
